69fa798201de3a108ad2d522e2315b65b8bcc8f5
Ankam Srinivas <M1064521@mindtree.com>
2021-05-20 16:46:08 +0530

    Add project files.

---
+++ GtCap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GtCap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
